35773c7f9c318c74c303e5f46972a2949b4af31e
Zarkyan <akihikozarkyan@gmail.com>
2016-11-26 23:24:29 +0100

    Update link + Script

--- script/t411_dl.user.js
+++ script/t411_dl.user.js
@@ -2,10 +2,10 @@
 // ==UserScript==
 // @name        T411 RapidDl
 // @namespace   t411
-// @description Ajoute un bouton de téléchargement rapide sur le site T411.ch
-// @include     http://www.t411.ch/torrents/search/*
-// @include		http://www.t411.ch/top/*
-// @version     1.3
+// @description Ajoute un bouton de téléchargement rapide sur le site T411.li
+// @include     http://www.t411.li/torrents/search/*
+// @include		http://www.t411.li/top/*
+// @version     1.4
 // @grant       none
 // ==/UserScript==
 
