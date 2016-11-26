502b0e253edac63131da3b176d95256ef7736870
Zarkyan <akihikozarkyan@gmail.com>
2016-11-26 02:30:59 +0100

    Update t411_dl.user.js

--- script/t411_dl.user.js
+++ script/t411_dl.user.js
@@ -1,1 +1,32 @@
 
+// ==UserScript==
+// @name        T411 Rapid DL :)
+// @namespace   t411
+// @description Ajoute un bouton de téléchargement rapide sur le site T411.li
+// @include     http://www.t411.li/torrents/search/*
+// @include		http://www.t411.li/top/*
+// @version     1.2
+// @grant       none
+// ==/UserScript==
+
+$.fn.addT411DownloadColumn = function() {
+  return this.each(function() {
+    var id = this.href.split('?')[1]; // Should be something like 'id=1234'
+    var newTdElement = $("<td></td>");
+    if( id != undefined && id.match(/id=[0-9]+/) ) {
+      var newLinkHtml = '<a href="/torrents/download/?' + id + '"><center><img src="http://img15.hostingpics.net/pics/151761download512000000.png"/></center></a>';
+      var newLinkElement = $(newLinkHtml);
+      newLinkElement.css("float","left");
+      newLinkElement.css("padding-left","16px");
+      newLinkElement.appendTo(newTdElement);
+    }
+    newTdElement.insertAfter($(this).parent());
+  });
+};
+
+// New distinct column header for downloading
+var newThElement = $("<th><center>DL</center></th>");
+newThElement.insertAfter( $('th:contains(NFO)') );
+
+// Insert all the buttons in the new column
+$('a.nfo').addT411DownloadColumn();
